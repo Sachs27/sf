9267ff0a2cbb44e1d5427bd8abcc5c766d5f2330
Sachs <sachswong27@gmail.com>
2013-10-14 16:00:20 +0800

    Update: Add sf_swap.

--- include/sf_utils.h
+++ include/sf_utils.h
@@ -5,6 +5,13 @@
 #include <inttypes.h>
 
 
+#define sf_swap(a, b) do {          \
+    typeof(a) __sf_swap_tmp__ = a;  \
+    a = b;                          \
+    b = __sf_swap_tmp__;            \
+} while (0)
+
+
 uint32_t sf_uint_next_power_of_two(uint32_t n);
 
 uint64_t sf_get_ticks(void);
